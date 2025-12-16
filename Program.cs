30bf226c414726e5227cacfa7aeca6efea67c9a2
lakshan-roshana <lakshanroshana21@gmail.com>
2025-12-16 10:35:42 +0530

    refactored locators to Xpath from Id

--- Program.cs
+++ Program.cs
@@ -28,15 +28,22 @@ namespace SauceBot
 
             // 3. INTERACT: Find the Username box and type 'problem_user'
             // We use By.Id because the developer gave this element an ID of "user-name"
-            IWebElement usernameField = driver.FindElement(By.Id("user-name"));
+            //IWebElement usernameField = driver.FindElement(By.Id("user-name"));
+
+            //try Xpath instead of Id
+            IWebElement usernameField = driver.FindElement(By.XPath("//input[@id='user-name']"));
             usernameField.SendKeys("problem_user");
 
             // 4. INTERACT: Find the Password box and type 'secret_sauce'
-            IWebElement passwordField = driver.FindElement(By.Id("password"));
+            //IWebElement passwordField = driver.FindElement(By.Id("password"));
+
+            //try Xpath instead of Id
+            IWebElement passwordField = driver.FindElement(By.XPath("//input[@id='password']"));
             passwordField.SendKeys("secret_sauce");
 
             // 5. ACTION: Click the Login Button
-            IWebElement loginButton = driver.FindElement(By.Id("login-button"));
+            //IWebElement loginButton = driver.FindElement(By.Id("login-button"));
+            IWebElement loginButton = driver.FindElement(By.XPath("//input[@id='login-button']"));
             loginButton.Click();
             Console.WriteLine("Clicked Login...");
 
@@ -60,7 +67,10 @@ namespace SauceBot
             // 1. Find the Price Element
             // On the website, the price text is inside a class called "inventory_item_price"
             // Since there are many items, this finds the FIRST one it sees.
-            IWebElement priceLabel = driver.FindElement(By.ClassName("inventory_item_price"));
+            //IWebElement priceLabel = driver.FindElement(By.ClassName("inventory_item_price"));
+
+            //try Xpath instead of ClassName
+            IWebElement priceLabel = driver.FindElement(By.XPath("//div[@class='inventory_item_price']"));
 
             // 2. Get the text from that element
             string actualPrice = priceLabel.Text;
@@ -80,18 +90,23 @@ namespace SauceBot
 
             // 1. Click the "Add to Cart" button
             // Note: We use the ID for the "Add" state, not the "Remove" state!
-            IWebElement addToCartBtn = driver.FindElement(By.Id("add-to-cart-sauce-labs-backpack"));
+            //IWebElement addToCartBtn = driver.FindElement(By.Id("add-to-cart-sauce-labs-backpack"));
+
+            //try Xpath instead of Id
+            IWebElement addToCartBtn = driver.FindElement(By.XPath("//button[@id='add-to-cart-sauce-labs-backpack']"));
             addToCartBtn.Click();
             
             // 2. Wait a moment for the animation
             //Thread.Sleep(1000);
-            wait.Until(d => d.FindElement(By.ClassName("shopping_cart_badge")));
+            //wait.Until(d => d.FindElement(By.ClassName("shopping_cart_badge")));
+            wait.Until(d => d.FindElement(By.XPath("//span[@class='shopping_cart_badge']")));
 
             // 3. Verify the Cart Badge "1" appeared
             // The red bubble has the class "shopping_cart_badge"
             try 
             {
-                IWebElement cartBadge = driver.FindElement(By.ClassName("shopping_cart_badge"));
+                //IWebElement cartBadge = driver.FindElement(By.ClassName("shopping_cart_badge"));
+                IWebElement cartBadge = driver.FindElement(By.XPath("//span[@class='shopping_cart_badge']"));
                 string itemsInCart = cartBadge.Text;
                 
                 if (itemsInCart == "1")
@@ -111,13 +126,15 @@ namespace SauceBot
             Console.WriteLine("Logging out...");
 
             // 1. Click the Menu
-            driver.FindElement(By.Id("react-burger-menu-btn")).Click();
+            //driver.FindElement(By.Id("react-burger-menu-btn")).Click();
+            driver.FindElement(By.XPath("//button[@id='react-burger-menu-btn']")).Click();
 
             // 2. Wait for animation (Crucial for sliding menus)
             Thread.Sleep(1000); 
 
             // 3. Find the Link
-            IWebElement logoutLink = driver.FindElement(By.Id("logout_sidebar_link"));
+            //IWebElement logoutLink = driver.FindElement(By.Id("logout_sidebar_link"));
+            IWebElement logoutLink = driver.FindElement(By.XPath("//a[@id='logout_sidebar_link']"));
 
             // --- THE FIX: FORCE CLICK (JavaScript) ---
             // This bypasses the UI and forces the browser to trigger the click event.
@@ -137,7 +154,7 @@ namespace SauceBot
             {
                 Console.WriteLine("❌ LOGOUT FAILED: URL never changed. Current: " + driver.Url);
             }
-            
+
             // 8. CLEANUP: Close the browser
             driver.Quit();
         }
